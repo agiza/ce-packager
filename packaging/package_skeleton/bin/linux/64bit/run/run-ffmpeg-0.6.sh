5e24ef351a071f876733bd2ec554fdf0bf7e1f88
KalturaGitBot <michael.dale@kaltura.com>
2013-02-13 19:12:13 +0000

    update from svn r93964

--- packaging/package_skeleton/bin/linux/64bit/run/run-ffmpeg-0.6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /etc/kaltura.d/system.ini
-
-KALTURA_BIN=$BASE_DIR/bin
-KALTURA_BIN_DIRS=$KALTURA_BIN
-KALTURA_BIN_FFMPEG=$KALTURA_BIN_DIRS/ffmpeg-0.6-dir
-LD_LIBRARY_PATH=$KALTURA_BIN_FFMPEG/lib $KALTURA_BIN_FFMPEG/ffmpeg "$@"
