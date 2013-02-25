0bc13b55719d45e84de36cc20680043d730b5b1e
KalturaGitBot <michael.dale@kaltura.com>
2013-02-25 15:02:51 +0000

    update from svn r94457

--- packaging/package.php
+++ packaging/package.php
@@ -3,37 +3,35 @@
 include_once('lib/utils.php');
 
 // get and validate arguments
-$usage_string = 'Usage is php '.__FILE__.' <outputdir> <preinstalled> <type> <number> <beta>'.PHP_EOL;
-$usage_string .= "<outputdir> = directory in which to create the package (if it does not start with a '/' the running directory is attached".PHP_EOL;
-$usage_string .= '<preinstalled> = true / false'.PHP_EOL;
-$usage_string .= '<type> = CE / TM'.PHP_EOL;
-$usage_string .= '<number> = vX.X.X'.PHP_EOL;
-$usage_string .= '<beta> = extra text to include in the version name (like "beta")'.PHP_EOL;
+$usage_string = 'Usage is php '.__FILE__.' <outputdir>'.PHP_EOL;
+$usage_string .= "<outputdir> = directory in which to create the package (if it does not start with a '/' the running directory will be attached)".PHP_EOL;
+//$usage_string .= '<preinstalled> = true / false'.PHP_EOL;
+//$usage_string .= '<type> = CE / TM'.PHP_EOL;
+//$usage_string .= '<number> = vX.X.X'.PHP_EOL;
+//$usage_string .= '<beta> = extra text to include in the version name (like "beta")'.PHP_EOL;
 
-if (count($argv) < 5) {
+if ($argc < 2) {
 	die($usage_string);
 }
 
 $version_ini = array();
 $version_ini['outputdir'] = trim($argv[1]);
-$version_ini['preinstalled'] = trim($argv[2]);
-$version_ini['type'] = trim($argv[3]);
-$version_ini['number'] = trim($argv[4]);
-$version_ini['beta'] = '';
-if (count($argv) > 5)
-	$version_ini['beta'] = trim($argv[5]);
+$version_ini['preinstalled'] = 'false';
+$version_ini['type'] = 'TM';
+$version_ini['number'] = '7.1.0';
+$version_ini['beta'] = 'beta';
 
-if (strcmp($version_ini['type'], 'CE') != 0 &&  strcmp($version_ini['type'], 'TM') != 0) {
-	die($usage_string);		
-}
-
-if (strcmp($version_ini['preinstalled'], 'true') != 0 && strcmp($version_ini['preinstalled'], 'false') != 0) {
-	die($usage_string);		
-}
-
-if (preg_match('/^v[0-9]+\.[0-9]+\.[0-9]+/', $version_ini['number']) != 1) {
-	die($usage_string);
-}
+//if (strcmp($version_ini['type'], 'CE') != 0 &&  strcmp($version_ini['type'], 'TM') != 0) {
+//	die($usage_string);
+//}
+//
+//if (strcmp($version_ini['preinstalled'], 'true') != 0 && strcmp($version_ini['preinstalled'], 'false') != 0) {
+//	die($usage_string);
+//}
+//
+//if (preg_match('/^v[0-9]+\.[0-9]+\.[0-9]+/', $version_ini['number']) != 1) {
+//	die($usage_string);
+//}
 
 // set php_ini settings
 error_reporting(E_ALL);
